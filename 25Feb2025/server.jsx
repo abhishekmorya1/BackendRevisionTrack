0379c9b952df90eb95fc6a3eeb720c53360eb6a8
Abhishek Morya <abhimorya8826@gmail.com>
2025-02-25 22:06:21 +0530

    25feb2025

--- 25Feb2025/server.jsx
+++ 25Feb2025/server.jsx
@@ -1,1 +1,12 @@
-// hello everyone
+// today is 25 feb 2025
+
+const express = require("express");
+const app=express();
+
+app.listen(4500, ()=>{
+    console.log("server is started at port no 4500");
+});
+
+app.get('/', (request,response)=> {
+    response.send("hey everyone this is first get");
+});
